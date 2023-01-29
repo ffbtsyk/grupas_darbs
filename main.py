acf718dc909c4aba71eb8fc45f0e7f0d55a71c33
ffbtsyk <123469324+ffbtsyk@users.noreply.github.com>
2023-01-29 11:34:46 +0000

    Iesku darbu

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 print("slinkais astrologs")
-#tests3
+import random
+
+zodiac = ["Aunus", "Dvini", "Jaunava", "Lauva", "Mazazis", "Skorpions", "Strelnieks", "Svari", "Udensvirs", "Versis", "Vezis", "Zivis"]
+horoskopi = ["prieks", "nauda", "skumjas", "milestiba", "veiksme", "jaunas iepazisanas", "naudas samazisanas", "liela zaudesana", "laime", "bagatiba", "iespejams daudz stressa", "laime"]
+
+random.shuffle(horoskopi)
+
