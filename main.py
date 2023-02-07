fdb127cdad51adb117d468784a00246642af6044
ldmissjana <123470348+ldmissjanaa@users.noreply.github.com>
2023-02-07 13:13:12 +0000

    izlaboju 1 laimi uz dusmas

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ print("slinkais astrologs")
 import random
 
 zodiac = ["Aunus", "Dvini", "Jaunava", "Lauva", "Mazazis", "Skorpions", "Strelnieks", "Svari", "Udensvirs", "Versis", "Vezis", "Zivis"]
-horoskopi = ["prieks", "nauda", "skumjas", "milestiba", "veiksme", "jaunas iepazisanas", "naudas samazisanas", "liela zaudesana", "laime", "bagatiba", "iespejams daudz stressa", "laime"]
+horoskopi = ["prieks", "nauda", "skumjas", "milestiba", "veiksme", "jaunas iepazisanas", "naudas samazisanas", "liela zaudesana", "laime", "bagatiba", "iespejams daudz stressa", "dusmas"]
 
 random.shuffle(horoskopi)
 
