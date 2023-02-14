600516f8a3ed817203c741d8c3b7920660a7a9b8
ldmissjana <123470348+ldmissjanaa@users.noreply.github.com>
2023-02-14 12:58:05 +0000

    labojums

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 print("slinkais astrologs")
 import random
 
-zodiac = ["Aunus", "Dvini", "Jaunava", "Lauva", "Mazazis", "Skorpions", "Strelnieks", "Svari", "Udensvirs", "Versis", "Vezis", "Zivis"]
+zodiac = ["Auns", "Dvini", "Jaunava", "Lauva", "Mezazis", "Skorpions", "Strelnieks", "Svari", "Udensvirs", "Versis", "Vezis", "Zivis"]
 horoskopi = ["prieks", "nauda", "skumjas", "milestiba", "veiksme", "jaunas iepazisanas", "naudas samazisanas", "liela zaudesana", "laime", "bagatiba", "iespejams daudz stressa", "dusmas"]
 
 random.shuffle(horoskopi)
